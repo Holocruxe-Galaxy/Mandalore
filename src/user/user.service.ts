5662f0015c0e52ba40fa2ea335a8651d62d48c63
EmiCon97 <derleuchtturm97@gmail.com>
2023-06-05 14:37:56 -0300

    testing 1

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -14,10 +14,23 @@ export class UserService {
     private userRepository: Repository<User>,
   ) {}
   async create(createUserDto: CreateUserDto) {
-    const group: Group = await this.groupRepository.insert(createUserDto.group);
+    const group: Group = this.groupRepository.create({
+      name: createUserDto.group,
+    });
 
-    const user = await this.userRepository.insert(createUserDto);
-    console.log(user);
+    this.userRepository.insert(group);
+
+    console.log(group);
+
+    const username = createUserDto.username;
+
+    const user = this.userRepository.create({ ...group, username });
+
+    console.log('first try: ', user);
+    await this.userRepository.insert(user);
+    const justCreated = await this.userRepository.findBy({ id: user.id });
+    console.log('second try: ', user);
+    console.log('third try: ', justCreated);
     return 'This action adds a new user';
   }
 
