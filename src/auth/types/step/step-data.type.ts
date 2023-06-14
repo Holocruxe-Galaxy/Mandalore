acdf19b22f47a92e59bdbf858e7fea6548297fd6
EmiCon97 <derleuchtturm97@gmail.com>
2023-06-14 14:22:06 -0300

    step

--- src/auth/types/step/step-data.type.ts
+++ src/auth/types/step/step-data.type.ts
@@ -1,9 +1,9 @@
 import { Step } from 'src/auth/interfaces';
 import { CreateContactInfoDto, CreatePersonalDto } from 'src/auth/dto';
 
-export const stepData = {
-  1: { name: 'contactInfo', dto: new CreateContactInfoDto() },
-  2: { name: 'personal', dto: new CreatePersonalDto() },
+export const stepData: Step = {
+  1: { name: 'contactInfo', dto: CreateContactInfoDto },
+  2: { name: 'personal', dto: CreatePersonalDto },
 } as const;
 
 export type StepDataKeys = keyof typeof stepData;
