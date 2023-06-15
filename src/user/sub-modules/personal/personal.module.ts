e2c37757aa3382f12a3846c8008969b1ef46d823
EmiCon97 <derleuchtturm97@gmail.com>
2023-06-15 03:21:32 -0300

    personal entity added to module

--- src/user/sub-modules/personal/personal.module.ts
+++ src/user/sub-modules/personal/personal.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonalService } from './personal.service';
 import { PersonalController } from './personal.controller';
+import { Personal } from './entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Personal])],
   controllers: [PersonalController],
   providers: [PersonalService],
   exports: [PersonalService],
