9a9870d0beaeabd3261239cef4d106160056b38b
EmiCon97 <derleuchtturm97@gmail.com>
2023-07-15 19:04:51 -0300

    fix: favorite function is now optional on update diary dto

--- src/diary/dto/update-diary.dto.ts
+++ src/diary/dto/update-diary.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDiaryDto } from './create-diary.dto';
-import { IsBoolean } from 'class-validator';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateDiaryDto extends PartialType(CreateDiaryDto) {
+  @IsOptional()
   @IsBoolean()
   favorite: boolean;
 }
