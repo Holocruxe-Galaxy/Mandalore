0db9f2296e3384d4dcf803f5ef2127ecb7ed3ef0
EmiCon97 <derleuchtturm97@gmail.com>
2023-07-15 04:06:51 -0300

    feat: diary schema added to module

--- src/diary/diary.module.ts
+++ src/diary/diary.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { DiaryService } from './diary.service';
 import { DiaryController } from './diary.controller';
 
+import { Diary, DiarySchema } from './schemas';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Diary.name,
+        schema: DiarySchema,
+      },
+    ]),
+  ],
   controllers: [DiaryController],
-  providers: [DiaryService]
+  providers: [DiaryService],
 })
 export class DiaryModule {}
