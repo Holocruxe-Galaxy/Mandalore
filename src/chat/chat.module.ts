519efbe8404bf32379a9459f58b77b8c50c2fbae
EmiCon97 <derleuchtturm97@gmail.com>
2023-07-24 04:02:09 -0300

    feat: chat schema added to module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { Chat, ChatSchema } from './entities';
 
 @Module({
-  providers: [ChatGateway, ChatService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Chat.name,
+        schema: ChatSchema,
+      },
+    ]),
+  ],
+  providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
