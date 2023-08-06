34a9dbd77c4ffd72424865060fad1a87996c7d09
EmiCon97 <derleuchtturm97@gmail.com>
2023-08-06 15:44:07 -0300

    fix: plan and status deleted from select file

--- src/user/interfaces/responses/all-user-response.interface.ts
+++ src/user/interfaces/responses/all-user-response.interface.ts
@@ -1,10 +1,8 @@
 import { ObjectId } from 'mongoose';
-import { PlanType, RoleType, StatusType } from 'src/user/types';
+import { StatusType } from 'src/user/types';
 
 export interface Select {
   _id: ObjectId;
   email: string;
-  plan: PlanType;
-  role: RoleType;
   status: StatusType;
 }
