e51bae3a67b2551ab540aa4fb6046918d5b2e4f3
EmiCon97 <derleuchtturm97@gmail.com>
2023-08-29 15:50:00 -0300

    added: images resource

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
+import { ImagesModule } from './images/images.module';
 
 @Module({
   providers: [CommonService],
   exports: [CommonService],
+  imports: [ImagesModule],
 })
 export class CommonModule {}
