43378de1af911df6121e1b9bd27df23b92ed9c8b
JaluGJ <gabriel.i.jalil@gmail.com>
2023-09-25 14:29:45 -0300

    refactor(swagger): Added api options to dto

--- src/user/form/dto/steps.dto.ts
+++ src/user/form/dto/steps.dto.ts
@@ -15,30 +15,36 @@ import {
   PersonalDto,
   ProfessionalDto,
 } from './';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class StepsDto {
   @IsOptional()
   @Type(() => ContactInfoDto)
   @ValidateNested()
+  @ApiProperty()
   contactInfo: ContactInfo;
 
   @IsOptional()
   @Type(() => GeneralInterestsDto)
   @ValidateNested()
+  @ApiProperty()
   generalInterests: GeneralInterests;
 
   @IsOptional()
   @Type(() => LocationDto)
   @ValidateNested()
+  @ApiProperty()
   location: Location;
 
   @IsOptional()
   @Type(() => PersonalDto)
   @ValidateNested()
+  @ApiProperty()
   personal: Personal;
 
   @IsOptional()
   @Type(() => ProfessionalDto)
   @ValidateNested()
+  @ApiProperty()
   professional: Professional;
 }
