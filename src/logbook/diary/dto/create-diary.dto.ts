271a639def5ae7d6619d1e9f4fbee47bce20a927
EmiCon97 <derleuchtturm97@gmail.com>
2023-10-30 18:08:54 -0300

    docs: diary example

--- src/logbook/diary/dto/create-diary.dto.ts
+++ src/logbook/diary/dto/create-diary.dto.ts
@@ -1,18 +1,18 @@
-import { IsArray, IsBoolean, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { diaryExample } from '../examples/diary.example';
 
 export class CreateDiaryDto {
   @IsString()
+  @ApiProperty({ type: String, example: diaryExample.content })
   content: string;
 
   @IsOptional()
   @IsString()
-  emoji: string;
-
-  @IsOptional()
-  @IsArray()
-  @IsString({ each: true })
-  photos: string[];
+  @ApiProperty({ type: String, example: diaryExample.emoji })
+  emoji?: string;
 
   @IsBoolean()
+  @ApiProperty({ type: String, example: diaryExample.favorite })
   favorite: boolean;
 }
---
+++ src/logbook/diary/examples/diary.example.ts
@@ -0,0 +1,7 @@
+import { CreateDiaryDto } from '../dto/create-diary.dto';
+
+export const diaryExample: CreateDiaryDto = {
+  content: 'I had a nice birthday! Today!',
+  favorite: false,
+  emoji: '😊',
+};
